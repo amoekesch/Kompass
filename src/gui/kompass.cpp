c24aba659a21d3ade9c7339d09e23a047ed36654
Andreas Moekesch <moekesch@gmail.com>
2021-11-14 09:45:17 -0500

    added server reload after cfg changes

--- src/gui/kompass.cpp
+++ src/gui/kompass.cpp
@@ -272,6 +272,9 @@ void Kompass::setupUi()
         dlgSettings->exec();
         ui->activateWindow();
         ui->raise();
+
+        setupDataTypes();
+        setupDataCountries();
         updatingStatus = false;
     });
 
