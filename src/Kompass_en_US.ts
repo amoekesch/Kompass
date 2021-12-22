294d7b56f547a1f0565d4e51fa715cb21b17e809
Andreas Moekesch <moekesch@gmail.com>
2021-12-22 17:27:46 -0500

    Release build v2.0

--- src/Kompass_en_US.ts
+++ src/Kompass_en_US.ts
@@ -24,7 +24,7 @@
         </message>
         <message>
             <source>appVersion</source>
-            <translation>v1.7</translation>
+            <translation>v2.0</translation>
         </message>
         <message>
             <source>appVersionUpgrade</source>
